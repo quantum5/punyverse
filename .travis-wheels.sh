f6fda9b31e77e2b16c396e315b72e7fbb4a231b1
Quantum <quantum2048@gmail.com>
2018-08-22 17:00:41 -0400

    Only attempt to make punyverse wheels.

--- .travis-wheels.sh
+++ .travis-wheels.sh
@@ -11,7 +11,7 @@ for PYBIN in /opt/python/*/bin; do
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/punyverse*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
