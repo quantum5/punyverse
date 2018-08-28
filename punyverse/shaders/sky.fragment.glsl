bbfacd55e45a21c314b86fd494b78b4bcd62270e
Quantum <quantum2048@gmail.com>
2018-08-27 23:21:34 -0400

    Orient the sky texture more correctly.

--- punyverse/shaders/sky.fragment.glsl
+++ punyverse/shaders/sky.fragment.glsl
@@ -5,5 +5,5 @@ out vec4 o_fragColor;
 uniform sampler2D u_skysphere;
 
 void main() {
-    o_fragColor = vec4(texture(u_skysphere, v_uv).rgb, 1);
+    o_fragColor = vec4(texture(u_skysphere, vec2(1 - v_uv.s, v_uv.t)).rgb, 1);
 }
