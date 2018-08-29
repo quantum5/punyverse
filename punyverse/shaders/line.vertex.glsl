510dcb46a680b42ce4ebf63ea999fc067fdacb0e
Quantum <quantum2048@gmail.com>
2018-08-28 20:34:10 -0400

    Convert orbits to shaders.

--- punyverse/shaders/line.vertex.glsl
+++ punyverse/shaders/line.vertex.glsl
@@ -1,8 +1,8 @@
 #version 130
 
-in vec2 a_position;
+in vec3 a_position;
 uniform mat4 u_mvpMatrix;
 
 void main() {
-    gl_Position = u_mvpMatrix * vec4(a_position, 0, 1);
+    gl_Position = u_mvpMatrix * vec4(a_position, 1);
 }
